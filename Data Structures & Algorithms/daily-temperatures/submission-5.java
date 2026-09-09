f70fd6fb31ec9d502176d849529e5370aad2bd6a
Satya Mahesh Kolli <satyamahesh.kolli03@gmail.com>
2026-09-09 15:55:57 +0530

    Add: daily-temperatures - submission-5

---
+++ Data Structures & Algorithms/daily-temperatures/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int N = temperatures.length;
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[N];
+
+        for(int temp = 0; temp<N; temp++){
+            for(int tempNext = temp+1; tempNext<N; tempNext++){
+                if(temperatures[tempNext]>temperatures[temp]){
+                    result[temp] = tempNext-temp;
+                    break;
+                }
+                if(tempNext==N-1){
+                    result[temp] = 0;
+                }
+            }
+        }
+
+        return result;
+    }
+}
